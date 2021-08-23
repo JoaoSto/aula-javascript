240e093d7ac5b09bcc333f165286297f77c9a895
JoaoSto <jvmunhoz@gmail.com>
2021-08-23 16:10:10 -0300

    Finalização Layout
    Implementação simples de JS dos efeitos mais comuns em uma página.

---
+++ js/main.js
@@ -0,0 +1,67 @@
+// var nome = "João";
+// var idade = 22;
+// var idade2 = 1;
+// var frase = "Lorem ipsum dolor sit amet."
+
+// alert(nome + " tem " + (idade - idade2) + " anos... por hora");
+
+// console.log(nome);
+// console.log(idade - idade2);
+// console.log(frase.replace("Lorem", "Merol"));
+// console.log(frase.toLocaleLowerCase());
+
+//-------------------------------------------------
+
+// var lista = ["a1", "a2", "a3", "a4"];
+
+// lista.push("a5");
+// lista.pop();
+// console.log(lista.reverse());
+
+// console.log(lista.toString());
+// console.log(lista.join(" - "));
+
+//-------------------------------------------------
+
+// var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}, {nome: "pêra", cor: "verde"}];
+
+// console.log(frutas);
+
+// alert(frutas[1].nome + " " + frutas[1].cor);
+
+//-------------------------------------------------
+
+// var idade = prompt("Qual sua idade?");
+
+// if (idade >= 18){
+//     alert("Maior de idade");
+// }
+
+// else{
+//     alert("Menor de idade");
+// }
+
+//-------------------------------------------------
+
+// var count = 0;
+// while(count<=10){
+//     console.log(count);
+//     count++;
+// }
+
+//-------------------------------------------------
+
+// var count;
+// for(count=1; count<=5; count++){
+//     alert(count);
+// };
+
+//-------------------------------------------------
+
+// var d = new Date();
+// alert(d.getHours());
+// alert(d.getMinutes());
+// alert(d.getDay());
+// alert(d.getMonth());
+
+//-------------------------------------------------
